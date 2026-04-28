c0239d149a02c0300df07f1e42d6f0f2b43b4af0
spiniandrea01-arch <spiniandrea01@gmail.com>
2026-04-28 13:35:15 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -12,6 +12,7 @@ const navItems = [
   { label: "Home", href: "#" },
   { label: "Appartamento", href: "#appartamento" },
   { label: "Servizi", href: "#servizi" },
+  { name: "Tariffe", href: "#prezzi" },
   { label: "Galleria", href: "#galleria" },
   { label: "Posizione", href: "#posizione" },
   { label: "Contatti", href: "#prenota" },
